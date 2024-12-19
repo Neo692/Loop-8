2d691aec4216e0dfb692f0fbf6eadf07df188ce1
nehy06001 <nehy06001@E-PW05CGQV>
2024-12-19 15:45:45 +0100

    Add project files.

---
+++ Loop 8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace loop8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int produkt = 1;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            produkt *= i;
+        }
+
+        Console.WriteLine("Produkten av heltalen från 1 till 5 är: " + produkt);
+    }
+}
